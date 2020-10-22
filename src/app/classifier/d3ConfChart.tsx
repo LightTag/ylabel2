5e9ef997c42a41708f2ea394e512d6024a2ba0b9
Tal Perry <tal@lighttag.io>
2020-10-22 19:25:11 +0200

    chart updates on validation

--- src/app/classifier/d3ConfChart.tsx
+++ src/app/classifier/d3ConfChart.tsx
@@ -13,7 +13,7 @@ import colorManaer from "app/utils/labelsetcolors/labelsetcolors";
 async function makeChart() {
   var margin = { top: 10, right: 30, bottom: 30, left: 60 };
   const width = 600 - margin.left - margin.right;
-  const height = 200 - margin.top - margin.bottom;
+  const height = 600 - margin.top - margin.bottom;
   var svg = select("#chart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -23,21 +23,27 @@ async function makeChart() {
 
   const data = await getRunResults();
   console.log(data);
-  var x = scaleLinear().domain([-1, 5]).range([0, width]);
+
+  const arrays = Object.values(data);
+  const firstObject = arrays[0];
+  if (firstObject === undefined) {
+    return;
+  }
+  var x = scaleLinear().domain([-1, firstObject.length]).range([0, width]);
   svg
     .append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(axisBottom(x));
 
   // Add Y axis
-  var y = scaleLinear().domain([0.5, 1.1]).range([height, 0]);
+  var y = scaleLinear().domain([-0.5, 1.1]).range([height, 0]);
   svg.append("g").call(axisLeft(y));
 
   Object.entries(data).forEach(([label, steps]) => {
     // Show confidence interval
     const lcolor = colorManaer.getLabelColor(label);
     const acolor = fade(lcolor, 0.25);
-    console.log(label, steps);
+    console.log(label, lcolor, steps);
 
     svg
       .append("path")
@@ -49,9 +55,9 @@ async function makeChart() {
         area<typeof steps extends Array<infer U> ? U : never>()
           .x((aa, ix) => x(ix) as number)
 
-          .y0((aa) => y(aa.lower || 0) as number)
+          .y0((aa) => y(aa.lower || Math.random() * 0.01) as number)
 
-          .y1((aa) => y(aa.upper || 0) as number)
+          .y1((aa) => y(aa.upper || Math.random() * 0.01) as number)
       );
 
     // Add the line
@@ -66,15 +72,22 @@ async function makeChart() {
 
         line<typeof steps extends Array<infer U> ? U : never>()
           .x((aa, ix) => x(ix) as number)
-          .y((aa) => y(aa.mean || 0) as number)
+          .y((aa) => y(aa.mean || Math.random() * 0.01) as number)
       );
   });
 }
 const D3Chart: FunctionComponent = () => {
+  const [refreshNum, setRefreshNum] = React.useState<number>(1);
   React.useEffect(() => {
     setTimeout(makeChart, 200);
-  }, [1]);
-  return <div id={"chart"} />;
+  }, [refreshNum]);
+  return (
+    <div
+      id={"chart"}
+      onClick={() => setRefreshNum(refreshNum + 1)}
+      key={refreshNum}
+    />
+  );
 };
 
 export default D3Chart;
