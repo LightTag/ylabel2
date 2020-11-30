7770e99e372be569f157df647f9dcfc2bf974bcd
Tal Perry <tal@lighttag.io>
2020-11-30 13:48:48 +0100

    fix bug with classifcation ribbon

--- src/frontend/components/example/ClassificationRibbon.tsx
+++ src/frontend/components/example/ClassificationRibbon.tsx
@@ -96,7 +96,7 @@ const ClassificationRibbon: FunctionComponent<Props> = React.memo((props) => {
   const example = useDatabase(
     ["exampleLabel", exampleId],
     "example",
-    (db) => db.example.get("exampleId"),
+    (db) => db.example.get(exampleId),
     props.exampleId
   );
 
